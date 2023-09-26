0b7f6aa22862bc037ae7c353b9b9c6bed3b341dc
Vadims Truhanovs <vtruhanovs@edu.riga.lv>
2023-09-26 16:08:05 +0300

    edit1

--- src/main.py
+++ src/main.py
@@ -15,6 +15,9 @@ screen = pygame.display.set_mode((HEIGHT, WIDTH))
 pygame.display.set_caption(TITLE)
 # pygame.display.set_icon(pygame.image.load(ICON))
 
+## Cursor
+pygame.mouse.set_cursor(*pygame.cursors.broken_x)
+
 ## Fonts init
 pygame.font.init()
 Fps_Font = pygame.font.SysFont('Cascadia Code', 25)
@@ -43,11 +46,11 @@ while running:
             running = False
             break
         
-    ## reset screen
+    ## reset screen (set black background)
     screen.fill((0, 0, 0))
     
-    ## Game logic update
-    game_manager.update()
+    ## Game logic and render update
+    game_manager.update(events)
     
     ## FPS counter
     screen.blit(Fps_Font.render(f"{int(clock.get_fps())}", True, (0, 255, 0)), (5, 5))
--- src/systems/GameManager.py
+++ src/systems/GameManager.py
@@ -3,5 +3,18 @@ import pygame
 class GameManager():
     def __init__(self):
         pass
-    def update(self):
-        pass
+    def update(self, events):
+        pygame.mouse.get_pressed(3)
+        for e in events:
+            # e.key == pygame.mouse.get_pressed
+            # e.type == pygame.MOUSEBUTTONDOWN
+            
+            # if e.key
+            if e.type == pygame.KEYDOWN:
+                if e.key == pygame.K_ESCAPE:
+                    print(True)
+                if e.key == pygame.K_RIGHT:
+                    print("pressed right")
+            if e.type == pygame.KEYUP:
+                if e.key == pygame.K_RIGHT:
+                    print("pressed right")
